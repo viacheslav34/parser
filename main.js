6e5bc38728f434f174c34561777d612b60e3655b
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2022-07-06 17:17:22 +0300

    сделал парсер

---
+++ main.js
@@ -0,0 +1,13 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+function getOrder() {
+axios.get('https://freelance.habr.com/tasks?page=1').then(data => {
+    const $ = cheerio.load(data.data)
+    let res = ''
+    $('#tasks_list > li:nth-child(1) > article > div > header > div.task__title > a').each((i, elem) => {
+        res += `${$(elem).text()}\n`
+    })
+    console.log(res)
+})
+}
+setInterval(getOrder , 1000*60)
