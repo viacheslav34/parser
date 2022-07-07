10947f4dc3128e0e52cb9d3211a09b0627704cb5
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2022-07-07 10:57:53 +0300

    gitignore

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 function getOrder() {
-axios.get('https://freelance.habr.com/tasks?page=1').then(data => {
+axios.get('https://freelance.habr.com/tasks?categories=development').then(data => {
     const $ = cheerio.load(data.data)
     let res = ''
     $('#tasks_list > li:nth-child(1) > article > div > header > div.task__title > a').each((i, elem) => {
